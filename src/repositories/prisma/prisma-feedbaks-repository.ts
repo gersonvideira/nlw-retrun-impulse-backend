be84287c605e9ce71351982f81e9ecdb64d25941
gersonvideira <23234311+gersonvideira@users.noreply.github.com>
2022-05-18 08:23:32 +0100

    Alterar caminho de aqruivos

--- src/repositories/prisma/prisma-feedbaks-repository.ts
+++ src/repositories/prisma/prisma-feedbaks-repository.ts
@@ -1,5 +1,5 @@
-import { client } from "@database/client";
-import { FeedbackCreateData, FeedbacksRepository } from "@repositories/feedbacks-repository";
+import { client } from "../../database/client";
+import { FeedbackCreateData, FeedbacksRepository } from "../../repositories/feedbacks-repository";
 
 export class PrismaFeedbacksRepository implements FeedbacksRepository {
   async create({ type, comment, screenshot }: FeedbackCreateData){
--- src/routes/feedback.routes.ts
+++ src/routes/feedback.routes.ts
@@ -1,7 +1,7 @@
-import { PrismaFeedbacksRepository } from '@repositories/prisma/prisma-feedbaks-repository'
+import { PrismaFeedbacksRepository } from '../repositories/prisma/prisma-feedbaks-repository'
 import { Router } from 'express'
-import { NodemailerMailAdapter } from 'src/adapters/nodemailer/nodemailer-mail-adapter'
-import { SubmitFeedbackUseCase } from 'src/use-cases/submit-feedback-use-case'
+import { NodemailerMailAdapter } from '../adapters/nodemailer/nodemailer-mail-adapter'
+import { SubmitFeedbackUseCase } from '../use-cases/submit-feedback-use-case'
 
 class FeedbackRoutes {
   static define(feedbackRoutes: Router): Router {
